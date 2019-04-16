bd497a72e4c3cea5b5ed6d1580a2fd9e11268ff8
Ubuntu <nithin@PracticeVM.emwhq4cpe4huzdezzhdhqkrrxf.tx.internal.cloudapp.net>
2019-04-16 07:20:18 +0000

    First commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+	client_id="5292526b-0a45-4b94-b1ac-77a893c84b7a"
+	client_secret="680d4d7c-f99d-4daa-a246-573559134f22"
+	subscription_id="999f6df5-8724-4d93-9ffe-e8a8bff727a0"
+	tenant_id="e06c8fea-edfd-499a-957a-7c2186cf7c22"
+}
+
+resource "azurerm_resource_group" "mynewrecgrp123" {
+	name="testing"
+	location="West US"
+}
